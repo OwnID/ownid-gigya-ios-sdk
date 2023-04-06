78c57a53aec6f0cca2877acd68e1599eee49d78c
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-04-06 17:04:06 -0500

    KYIV-1874: Instant Connect Journeys (#18)
    * Update OwnIDGigyaSDK.swift
    
    * Update OwnIDGigyaSDK.swift
    
    * Update OwnIDGigyaSDK.swift
    
    * Update OwnIDGigyaSDK.swift
    
    * Update OwnIDGigyaSDK.swift
    
    * Update OwnIDGigyaSDK.swift
    
    * Update OwnIDGigyaSDK.swift
    
    * Update OwnIDGigyaSDK.swift
    
    * Add popup v1
    
    * Add present/dismiss view
    
    * Simplify public api
    
    * Update OwnIDGigyaSDK.swift

--- Core/OwnIDGigyaSDK.swift
+++ Core/OwnIDGigyaSDK.swift
@@ -83,5 +83,18 @@ public extension OwnID {
                                            visualConfig: OwnID.UISDK.VisualLookConfig = .init()) -> OwnID.FlowsSDK.LoginView {
             OwnID.FlowsSDK.LoginView(viewModel: viewModel, visualConfig: visualConfig)
         }
+        
+        public static func instantConncectViewModel<T: GigyaAccountProtocol>(instance: GigyaCore<T>,
+                                                                             sdkConfigurationName: String = sdkName) -> OwnID.FlowsSDK.LoginView.ViewModel {
+            loginViewModel(instance: instance, emailPublisher: Just("").eraseToAnyPublisher())
+        }
+        
+        public static func showInstantConnectView(viewModel: OwnID.FlowsSDK.LoginView.ViewModel,
+                                                  sdkConfigurationName: String = sdkName,
+                                                  visualConfig: OwnID.UISDK.VisualLookConfig = .init()) {
+            OwnID.UISDK.showInstantConnectView(viewModel: viewModel,
+                                               sdkConfigurationName: sdkConfigurationName,
+                                               visualConfig: visualConfig)
+        }
     }
 }
