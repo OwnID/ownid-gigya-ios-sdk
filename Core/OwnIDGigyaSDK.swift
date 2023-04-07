388f6ef463da4fd617fad9e76c4956dd70aa0504
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-04-07 10:58:20 -0500

    Simplify instant connect API

--- Core/OwnIDGigyaSDK.swift
+++ Core/OwnIDGigyaSDK.swift
@@ -84,11 +84,6 @@ public extension OwnID {
             OwnID.FlowsSDK.LoginView(viewModel: viewModel, visualConfig: visualConfig)
         }
         
-        public static func instantConncectViewModel<T: GigyaAccountProtocol>(instance: GigyaCore<T>,
-                                                                             sdkConfigurationName: String = sdkName) -> OwnID.FlowsSDK.LoginView.ViewModel {
-            loginViewModel(instance: instance, emailPublisher: Just("").eraseToAnyPublisher())
-        }
-        
         public static func showInstantConnectView(viewModel: OwnID.FlowsSDK.LoginView.ViewModel,
                                                   sdkConfigurationName: String = sdkName,
                                                   visualConfig: OwnID.UISDK.VisualLookConfig = .init()) {
